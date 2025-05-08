3b9d7d5edc0215557633ce14e4e07b70b0de631b
dandikamarthachristiyo <dandika.martha@gmail.com>
2025-05-08 08:23:36 +0700

    percobaan 1 lengkap

--- jobsheet_9/MahasiswaDemo07.java
+++ jobsheet_9/MahasiswaDemo07.java
@@ -12,6 +12,8 @@ public class MahasiswaDemo07 {
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Melihat Tugas Pertama Dikumpulkan");
+            System.out.println("6. Lihat Jumlah Tugas");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine();
@@ -27,7 +29,7 @@ public class MahasiswaDemo07 {
                     stack.push(mhs);
                     System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
                     break;
-                    case 2:
+                case 2:
                     Mahasiswa07 dinilai = stack.pop();
                     if (dinilai != null) {
                         System.out.println("Menilai tugas dari " + dinilai.nama);
@@ -47,10 +49,19 @@ public class MahasiswaDemo07 {
                     System.out.println("Daftar semua tugas");
                     System.out.println("Nama\tNIM\tKelas");
                     stack.print();
+                    break;
+                case 5:
+                    Mahasiswa07 terbawah = stack.peekBottom();
+                    if (terbawah != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh " + terbawah.nama);
+                    }
+                    break;
+                case 6:
+                    System.out.println("Jumlah tugas yang sudah dikumpulkan: " + stack.jumlahTugas());
                     break;            
                 default:
                     System.out.println("Pilihan tidak valid.");
             }
-        } while (pilih >= 1 && pilih  <= 4);
+        } while (pilih >= 1 && pilih  <= 6);
     }
 }
--- jobsheet_9/StackTugasMahasiswa07.java
+++ jobsheet_9/StackTugasMahasiswa07.java
@@ -50,9 +50,24 @@ public class StackTugasMahasiswa07 {
         }
     }
     public void print() {
-        for (int i = 0; i <= top; i++) {
+        for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
         }
         System.out.println("");
+    }
+    public Mahasiswa07 peekBottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+    public int jumlahTugas() {
+        if (!isEmpty()) {
+            return top + 1; 
+        } else {
+            return 0;
+        }
     }
 }
